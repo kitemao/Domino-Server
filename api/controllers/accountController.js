3b974e30e1bd07bb6132000eef41f4b23663098a
Zeke Zhao | 赵望野 <wangye.zhao@wandoujia.com>
2014-02-27 20:02:59 +0800

    Add auth API.

--- api/controllers/accountController.js
+++ api/controllers/accountController.js
@@ -1,35 +1,37 @@
+var gapis = require('googleapis');
+
+var CLIENT_ID = '389328904191.apps.googleusercontent.com';
+var CLIENT_SECRET = '3jTDE6VQJmr6RtUkTkA4x9hb';
+
+// TODO: Replace with production URL
+var REDIRECT_URL = 'http://127.0.0.1:1337/account/auth';
+
+var oauth2Client = new gapis.OAuth2Client(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL);
+
 module.exports = {
-    login : function  (req, res) {
-        console.log(req.cookies.dominoAuth);
-        if (req.cookies.dominoAuth) {
-            res.send({
-                status : 200
+    auth : function  (req, res) {
+        var tokens = req.body;
+
+        tokens.expires_in = parseInt(tokens.expires_in, 10);
+
+        oauth2Client.setCredentials(tokens);
+
+        gapis
+            .discover('plus', 'v1')
+            .execute(function(err, client) {
+                client
+                    .plus.people.get({ userId: 'me' })
+                    .withAuthClient(oauth2Client)
+                    .execute(function (err, response, body) {
+                        if (response.domain === 'wandoujia.com') {
+                            res.send(200);
+                        } else {
+                            res.send(403);
+                        }
+                    });
             });
-        } else {
-            var username = req.param('username');
-            var password = req.param('password');
-
-            // TODO Implement LDAP auth below
-            if (username !== 'wangye.zhao') {
-                res.send({
-                    status : 404
-                });
-            } else {
-                res.cookie('dominoAuth', new Date().getTime(), {
-                    maxAge : 1000 * 60 * 1,
-                    httpOnly : true
-                });
-
-                res.send({
-                    status : 200
-                });
-            }
-        }
     },
     logout : function (req, res) {
-        res.clearCookie('dominoAuth');
-        res.send({
-            status : 200
-        });
+
     }
 };
