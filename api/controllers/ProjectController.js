4112aba482dc5a0deb080e72f71fa1445522e6d0
Zeke Zhao | 赵望野 <wangye.zhao@wandoujia.com>
2014-03-13 22:05:09 +0800

    Add complete logic.

--- api/controllers/ProjectController.js
+++ api/controllers/ProjectController.js
@@ -7,6 +7,19 @@ var newProjectTpl = ejs.compile(fs.readFileSync(__dirname + '/../../views/misc/p
     encoding : 'utf8'
 }));
 
+var updateBuildingScriptAsync = function (data) {
+    var script = newProjectTpl({
+        title : data.title,
+        servers : data.stagingServer,
+        receivers : data.notificationList.map(function (item) {
+            return item + '@wandoujia.com';
+        }),
+        url : data.url
+    });
+
+    // TODO
+};
+
 module.exports = {
     _config : {},
     get : function () {
@@ -18,44 +31,43 @@ module.exports = {
         data.productionServer = data.productionServer.split('|');
         data.notificationList = data.notificationList.split('|');
 
-
-        console.log(newProjectTpl({
-            title : data.title,
-            servers : data.stagingServer,
-            receivers : data.notificationList.map(function (item) {
-                return item + '@wandoujia.com';
-            }),
+        Project.findOne({
             url : data.url
-        }));
-
-        // Project.findOne({
-        //     url : data.url
-        // }).done(function (err, project) {
-        //     if (project !== undefined) {
-        //         res.send({
-        //             status : StatusCode.RESOURCE_DUPLICATED,
-        //             err : {
-        //                 msg : 'REPO_ALREADY_EXISTS'
-        //             }
-        //         });
-        //     } else {
-        //         Project.create({
-        //             title : data.title,
-        //             description : data.description,
-        //             url : data.url,
-        //             type : parseInt(data.type, 10),
-        //             stagingServer : data.stagingServer.split('|'),
-        //             productionServer : data.productionServer.split('|'),
-        //             notificationList : data.notificationList.split('|')
-        //         }).done(function (err, project) {
-        //             if (!err) {
-        //                 res.send({
-        //                     status : StatusCode.SUCCESS,
-        //                     body : project
-        //                 });
-        //             }
-        //         });
-        //     }
-        // });
+        }).done(function (err, project) {
+            if (project !== undefined) {
+                res.send({
+                    status : StatusCode.RESOURCE_DUPLICATED,
+                    err : {
+                        msg : 'REPO_ALREADY_EXISTS'
+                    }
+                });
+            } else {
+                updateBuildingScriptAsync(data).done(function () {
+                    Project.create({
+                        title : data.title,
+                        description : data.description,
+                        url : data.url,
+                        type : parseInt(data.type, 10),
+                        stagingServer : data.stagingServer.split('|'),
+                        productionServer : data.productionServer.split('|'),
+                        notificationList : data.notificationList.split('|')
+                    }).done(function (err, project) {
+                        if (!err) {
+                            res.send({
+                                status : StatusCode.SUCCESS,
+                                body : project
+                            });
+                        }
+                    });
+                }).fail(function (err) {
+                    res.send({
+                        status : StatusCode.COMMUNICATION_WITH_THIRDPARTY_FAILED,
+                        err : {
+                            msg : err.msg
+                        }
+                    });
+                });
+            }
+        });
     }
 };
