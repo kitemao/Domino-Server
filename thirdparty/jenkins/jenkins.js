38c2a8ea08bea4151a337feb03195eda57359007
Zeke Zhao | 赵望野 <wangye.zhao@wandoujia.com>
2014-03-21 01:06:08 +0800

    Add Jenkins APIs.

--- thirdparty/jenkins/jenkins.js
+++ thirdparty/jenkins/jenkins.js
@@ -4,11 +4,22 @@ var ejs = require('ejs');
 var Q = require('q');
 var request = require('request');
 
-var newJenkinsJobTpl = ejs.compile(fs.readFileSync(__dirname + '/../views/misc/jenkinsJob.ejs', {
+var newJenkinsJobTpl = ejs.compile(fs.readFileSync(__dirname + '/jenkinsJob.ejs', {
     encoding : 'utf8'
 }));
 
+var API_PREFIX = 'http://10.0.22.110:8080';
 
+var auth = {
+    user : 'wangye.zhao',
+    pass : 'Zwy13603614886'
+};
+
+requestJenkins = function () {
+    arguments[0].url = API_PREFIX + arguments[0].url;
+
+    request.apply(this, arguments);
+};
 
 module.exports = {
     createJobAsync : function (data) {
@@ -20,15 +31,12 @@ module.exports = {
             taskName : 'deploy-staging'
         });
 
-        request({
+        requestJenkins({
             method : 'POST',
-            url : 'http://10.0.22.110:8080/createItem',
-            auth : {
-                user : 'wangye.zhao',
-                pass : 'Zwy13603614886'
-            },
+            url : '/createItem',
+            auth : auth,
             qs : {
-                name : 'Domino-Test'
+                name : 'data.title'
             },
             headers : {
                 'Content-Type' : 'text/xml'
@@ -47,7 +55,19 @@ module.exports = {
     deleteJobAsync : function () {
         // TODO
     },
-    runJobAsync : function () {
-        // TODO
+    runJobAsync : function (jobName) {
+        var deferred = Q.defer();
+
+        requestJenkins({
+            method : 'POST',
+            url : '/' + jobName + '/build',
+            auth : auth
+        }, function (err, res, body) {
+            if (res.statusCode === 200) {
+                deferred.resolve(body);
+            } else {
+                deferred.reject(new Error(body));
+            }
+        });
     }
 };
