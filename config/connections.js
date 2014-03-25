0fc93e681e302dfece668328d556c376f7d58e9a
Wangye Zhao | 赵望野 <wangye.zhao@wandoujia.com>
2014-03-25 11:09:15 +0800

    Test Travis.

--- config/connections.js
+++ config/connections.js
@@ -1,11 +1,11 @@
 module.exports.connections = {
     mongo : {
-      adapter : 'sails-mongo',
-      host : 'localhost',
-      port : 27017,
-      user : '',
-      password : '',
-      database : 'Domino'
+        adapter : 'sails-mongo',
+        host : '127.0.0.1',
+        port : 27017,
+        user : '',
+        password : '',
+        database : 'Domino'
     }
 };
 
