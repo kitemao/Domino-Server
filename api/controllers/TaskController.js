031dba625237859ad4573cfa9066437aa75014bb
Wangye Zhao | 赵望野 <wangye.zhao@wandoujia.com>
2014-04-17 22:41:32 +0800

    Add pagination for task list API.

--- api/controllers/TaskController.js
+++ api/controllers/TaskController.js
@@ -3,11 +3,15 @@ var StatusCode = require('../../utils/StatusCodeMapping');
 module.exports = {
     find : function (res, req) {
         var title = res.param('title');
+        var pageSize = res.param('pageSize') || 20;
+        var page = res.param('page') || 1;
 
         if (title) {
             Task.find({
                 projectTitle : title
-            }).sort('startTime DESC')
+            }).limit(pageSize)
+                .skip(Math.max(page - 1, 1))
+                .sort('startTime DESC')
                 .then(function (tasks) {
                     req.json({
                         body : tasks
