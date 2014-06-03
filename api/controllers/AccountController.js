73ce282a4f4503773505198585b4b1071dc296d4
miaojian <kitemao@gmail.com>
2014-06-03 14:12:38 +0800

    remove google Auth(temporary)
    Signed-off-by: miaojian <kitemao@gmail.com>

--- api/controllers/AccountController.js
+++ api/controllers/AccountController.js
@@ -4,20 +4,31 @@ module.exports = {
     auth: function  (req, res) {
         var tokens = req.body;
 
-        // 服务器访问google oauth 经常抽风，遂先去掉，默认为FE
-        //gplusSignIn.authAsync(req.body).then(function (user) {
-        res.send(200);
+        // 服务器访问google oAuth 经常抽风，遂先去掉，从前端获取
+        // gplusSignIn.authAsync(req.body).then(function (user) {
 
-        //    var accountName = _.find(user.emails, function (e) {
-        //        return e.type === 'account';
-        //    }).value.split('@')[0];
+            // var accountName = _.find(user.emails, function (e) {
+            //     return e.type === 'account';
+            // }).value.split('@')[0];
+
+        var accountName = req.body.accountName;
+        var displayName = req.body.displayName;
 
         req.session.authenticated = true;
-        req.session.accountName = 'FE';
+        req.session.accountName = accountName;
+
+        //var displayName = user.displayName;
+
+        res.send({
+            body: {
+                accountName: accountName,
+                displayName: displayName
+            }
+        }, 200);
 
         User.addAsync({
-            accountName: 'FE',
-            displayName: 'FE'
+            accountName: accountName,
+            displayName: displayName
         });
         // }, function (err) {
         //     res.send({
