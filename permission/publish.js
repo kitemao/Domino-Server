0c144c24d71280dc3271d0bd34ae95f3b059664e
miaojian <kitemao@gmail.com>
2014-06-03 14:14:52 +0800

    add white list Auth
    Signed-off-by: miaojian <kitemao@gmail.com>

---
+++ permission/publish.js
@@ -0,0 +1,25 @@
+/*
+ * 此系统的发布准入白名单
+ *
+ */
+module.exports = {
+
+    development: [
+        'miaojian',
+        'wangye.zhao'
+    ],
+
+    production: [
+        'miaojian',
+        'wangye.zhao',
+        'lixiaomeng',
+        'miaoziming',
+        'hanjiyun',
+        'wangxiao',
+        'gaohailang',
+        'leplay',
+        'lixiaopeng',
+        'zhailin',
+        'zhangyaochun'
+    ]
+};
