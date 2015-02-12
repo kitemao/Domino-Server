98a20776f5c7460bb814eb6c503ffaff8ea6b8a3
miaojian <miaojian@wandoujia.com>
2015-02-12 16:07:38 +0800

    add mysql
    Signed-off-by: miaojian <miaojian@wandoujia.com>

--- config/connections.js
+++ config/connections.js
@@ -6,6 +6,13 @@ module.exports.connections = {
         user : '',
         password : '',
         database : 'Domino'
+    },
+    someMysqlServer: {
+        adapter: 'sails-mysql',
+        host: 'YOUR_MYSQL_SERVER_HOSTNAME_OR_IP_ADDRESS',
+        user: 'YOUR_MYSQL_USER',
+        password: 'YOUR_MYSQL_PASSWORD',
+        database: 'YOUR_MYSQL_DB'
     }
 };
 
